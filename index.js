90f656d348e094a53eba08b0bc975f1d52f41e8a
uvenil <uvenil@web.de>
2018-07-01 15:52:41 +0200

    Farbiger helpstr, Par. trace

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-module.exports = (ausgabe = "", stringify = true, calldepth = 2, pathdepth = 2) => {
+module.exports = (ausgabe = "", trace = true, stringify = true, calldepth = 2, pathdepth = 2) => {
   // Errorobjekt erstellen und Callstack herauslesen
   function getErrorObject() { // Error-Objekt erzeugen
     try { throw Error('') } catch (err) { return err; }
@@ -18,10 +18,43 @@ module.exports = (ausgabe = "", stringify = true, calldepth = 2, pathdepth = 2) 
     return s;
   }, "");
   // Ausgabe
-  if (stringify) {
-    ausgabe = JSON.stringify(ausgabe);
-  }
-  ausgabe = `> ${ausgabe}${helpstack}`;
-  console.log(ausgabe);
-  return ausgabe;
+  const bright = "\x1b[1m"; // Farb-, Steuer-Codes
+  const dim = "\x1b[2m";
+  const reset = "\x1b[0m";
+  const magenta = "\x1b[35m";
+
+  if (stringify)  ausgabe = JSON.stringify(ausgabe);
+  const helpstr = magenta + `> ${bright}${ausgabe}${dim}${helpstack}` + reset;
+  if (trace) console.log(helpstr);
+  return helpstr;
 };
+
+/*
+Colors reference:
+
+Reset = "\x1b[0m"
+Bright = "\x1b[1m"
+Dim = "\x1b[2m"
+Underscore = "\x1b[4m"
+Blink = "\x1b[5m"
+Reverse = "\x1b[7m"
+Hidden = "\x1b[8m"
+
+FgBlack = "\x1b[30m"
+FgRed = "\x1b[31m"
+FgGreen = "\x1b[32m"
+FgYellow = "\x1b[33m"
+FgBlue = "\x1b[34m"
+FgMagenta = "\x1b[35m"
+FgCyan = "\x1b[36m"
+FgWhite = "\x1b[37m"
+
+BgBlack = "\x1b[40m"
+BgRed = "\x1b[41m"
+BgGreen = "\x1b[42m"
+BgYellow = "\x1b[43m"
+BgBlue = "\x1b[44m"
+BgMagenta = "\x1b[45m"
+BgCyan = "\x1b[46m"
+BgWhite = "\x1b[47m"
+*/
--- index.test.js
+++ index.test.js
@@ -5,11 +5,11 @@ const c = { "a": 2 };
 
 // Aufruf
 const res = hs(c)
-const check = '> {"a":2} < helpstack/index.test.js:7:13 < cjs/loader.js:678:30';
+const helpstr = '\u001b[35m> \u001b[1m{\"a\":2}\u001b[2m < helpstack/index.test.js:7:13 < cjs/loader.js:678:30\u001b[0m';
 
 // Test
 console.log('--- Start tests');
-if (res == check) {
+if (res == helpstr) {
   console.log(`${package} ok.`);
 } else {
   throw new Error(`${package} funktioniert nicht!`);
